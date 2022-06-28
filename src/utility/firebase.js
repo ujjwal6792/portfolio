50e3cd76a969ed0a57fa9666186733ac09be44c5
ujjwal6792 <ujjwalsharma.6792@gmail.com>
2022-06-28 23:54:27 +0530

    initial release

---
+++ src/utility/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBuf6sfYVZCE-i1HkwyA85LMZsHB-reRzc",
+  authDomain: "ujjwal-sharma.firebaseapp.com",
+  projectId: "ujjwal-sharma",
+  storageBucket: "ujjwal-sharma.appspot.com",
+  messagingSenderId: "232247333234",
+  appId: "1:232247333234:web:0ccd0228b60e3824858deb",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
