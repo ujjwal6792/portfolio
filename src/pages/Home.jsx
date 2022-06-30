09d14003f711f2a6923175efb6dc265bc1e0d0aa
ujjwal6792 <ujjwalsharma.6792@gmail.com>
2022-07-01 00:37:43 +0530

    added illustration

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from "../style/Home.module.css"
 import {FcMindMap} from "react-icons/fc"
+import Clip from "../images/clip.webp"
 
 function Home() {
   return (
@@ -9,7 +10,9 @@ function Home() {
       <div className={styles.title}>
         <h3>Hello. <FcMindMap/> <br/> I am Ujjwal</h3>
       </div>
-      <div className={styles.graphic}></div>
+      <div className={styles.graphic}>
+        <img src={Clip} alt="" />
+      </div>
       <div className={styles.button}></div>
     </div>
 
